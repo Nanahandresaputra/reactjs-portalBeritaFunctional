dfc15c83a49a253694ed75af12c599e03c19d168
Nanahandresaputra <nanaggmu@gmail.com>
2023-03-31 23:56:25 +0700

    modif 2nd

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
-export let search = "https://gnews.io/api/v4/search?q=";
-export let apiKey = "f778d893312b0a0462301097aa73a819";
-export let andApi = "&apikey";
-export let home = `https://gnews.io/api/v4/top-headlines?category=`;
-// https://gnews.io/api/v4/top-headlines?category=general&apikey=f778d893312b0a0462301097aa73a819
-// https://gnews.io/api/v4/search?q=example&apikey=f778d893312b0a0462301097aa73a819
+import config from "../config";
+export let search = config.search;
+export let apiKey = config.apikey;
+export let andApi = config.andApi;
+export let home = config.home;
+
+// f778d893312b0a0462301097aa73a819
---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  search: process.env.REACT_APP_SEARCH,
+  home: process.env.REACT_APP_HOME,
+  andApi: process.env.REACT_APP_ANDAPI,
+  apikey: process.env.REACT_APP_APIKEY,
+};
+
+export default config;
