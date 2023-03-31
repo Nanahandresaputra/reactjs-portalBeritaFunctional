ed6a99ab72b5c2320e63ecedd8dedc85977b7531
Nanahandresaputra <nanaggmu@gmail.com>
2023-04-01 03:42:06 +0700

    testing22

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,7 @@
-// import config from "../config";
-// export let search = config.search;
-// export let apiKey = config.apikey;
-// export let andApi = config.andApi;
-// export let home = config.home;
-export let search = "https://gnews.io/api/v4/search?q=";
-export let apiKey = "=a286f453ac05a6469b020f650e74c4fa";
-export let andApi = "&apikey";
-export let home = "https://gnews.io/api/v4/top-headlines?category=";
+import config from "../config";
+export let search = config.search;
+export let apiKey = config.apikey;
+export let andApi = config.andApi;
+export let home = config.home;
 
 // f778d893312b0a0462301097aa73a819
